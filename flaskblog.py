e50f3c0e181e06a0c485b041e15de51850a96aca
Sajo-riyo <106347006+Sajo-riyo@users.noreply.github.com>
2024-11-18 22:32:23 +0100

    Created login and registration classes in form.py, added validation to validate the creation of a user and worked on the register and login pages.

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '2eb2bfb49f5fb7901157f568fb252d5c'
 posts = [{'author': 'Jason Jang',
           'title': 'Aafari',
           'content': 'The Enfant Terrible',
@@ -23,7 +24,28 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html', ti)
+    return render_template('about.html', title='about')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
