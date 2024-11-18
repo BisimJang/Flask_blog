557b564ff238a8bdde70f75a54fb24c35e64088b
Sajo-riyo <106347006+Sajo-riyo@users.noreply.github.com>
2024-11-18 19:40:09 +0100

    Added a temporary database, styled the html files using bootstrap. Discovered Layouts and Jinja2.

--- flaskblog.py
+++ flaskblog.py
@@ -1,19 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [{'author': 'Jason Jang',
+          'title': 'Aafari',
+          'content': 'The Enfant Terrible',
+          'date_posted': '22nd Nov, 2024'
+          }
+    ,
+         {'author': 'Z',
+          'title': 'Draw, Damn it',
+          'content': 'An artistic monologue',
+          'date_posted': '13th Nov, 2024'
+          }]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html', ti)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
