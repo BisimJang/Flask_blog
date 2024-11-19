7db7fee340f6c10ba741342d0646ebf2c8161a36
Sajo-riyo <106347006+Sajo-riyo@users.noreply.github.com>
2024-11-19 15:47:36 +0100

    Working with the flask sqlalchemy module to create a database(Temporary), created an instance of the SQLAlchemy class

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2eb2bfb49f5fb7901157f568fb252d5c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
 posts = [{'author': 'Jason Jang',
           'title': 'Aafari',
           'content': 'The Enfant Terrible',
