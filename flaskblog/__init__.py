d41f26603572594eb5fe60ee0ca87b9c82b783c9
Sajo-riyo <106347006+Sajo-riyo@users.noreply.github.com>
2024-11-22 23:38:53 +0100

    Adding user authentication and data encryption. did a lot honestly, added new routes and import statements, should start working on a project to understand better. But ultimately have a better understanding.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2eb2bfb49f5fb7901157f568fb252d5c'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
