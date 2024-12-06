34bccb514016ab2f98f94712e193366cb7eabde7
Sajo-riyo <106347006+Sajo-riyo@users.noreply.github.com>
2024-12-06 18:28:20 +0100

    Now people are able to update they're account information, but not the password. Improved the account route, 'UpdateAccountForm' and account.html file.

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from flaskblog.models import User
@@ -34,3 +36,24 @@ class LoginForm(FlaskForm):
                              validators=[DataRequired()])
     remember = BooleanField()
     submit = SubmitField('Login In')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField("Username",
+                           validators=[DataRequired(), Length(2, 20)])
+    email = StringField("Email",
+                        validators=[DataRequired(), Email()])
+    #picture = FileField('Update Profile Picture.', validators=[FileAllowed()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one')
