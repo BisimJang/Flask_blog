072feee26c43a5e255839737ec8ce8c01e48f035
Sajo-riyo <106347006+Sajo-riyo@users.noreply.github.com>
2025-01-26 00:23:35 +0100

    [init.py]-Imported the Mail class from flask mail [form.py]- Created the form for resetting the password and its request form too [html files]- updated the login.html and created 2 new pages [models.py]-Used the itsdangerous module to generate unique time bound tokens to verify the request under the User model [routes.py]- created corresponding routes for the added html file for the requesting and resetting the password

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2eb2bfb49f5fb7901157f568fb252d5c'
@@ -11,5 +16,13 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
 
 from flaskblog import routes
